1833a78a4c1e29084a288fe4537e806bd2fddaa5
Win11 <tarkankonakci@gmail.com>
2024-05-14 21:48:02 +0300

    feat: create skills model

--- myCV/models.py
+++ myCV/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -81,5 +82,24 @@ class ImageSetting(AbstractModel):
     class Meta:
         verbose_name = 'Image Setting'
         verbose_name_plural = 'Image Settings'
-        ordering = ('name',)
+        ordering = ('name', )
+
+
+class Skill(AbstractModel):
+    order = models.IntegerField(
+        default=0,
+        verbose_name='Order',
+    )
+    name = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name='Name',
+        help_text='This is variable of the setting',
+    )
+    percentage = models.IntegerField(
+        default=50,
+        verbose_name='Percentage',
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+    )
 
